b24462be16ed9a38013cbca4ac872400825d01cd
Jared Silver <contactjaredsilver@gmail.com>
2016-10-02 11:23:43 -0400

    plz work

--- server/server.py
+++ server/server.py
@@ -11,26 +11,16 @@ from datetime import timedelta
 
 @app.route("/")
 def index():
-	#using a user_id 0
-   	db.add_goal('sleep', 0, 'hours', 8)
-   	goals = db.select_goals(0)
-
-    # results = db.select_one()
-    # print(results)
     return render_template("index.html")
 
 @app.route("/signup")
 def signup():
-
     return render_template("signup.html")
 
 @app.route("/member")
 def member():
-
     return render_template("member.html")
 
 if __name__ == "__main__":
 	app.debug = True
 	app.run('0.0.0.0')
-
-#
